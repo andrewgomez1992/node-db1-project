8443470a1e61be5d485aaf72fa8c181832f30fa8
Andrew Gomez <drewgomez209@gmail.com>
2022-06-13 13:27:17 -0700

    wired

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,54 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
+// const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    res.json('get accounts')
+    // const data = await Account.getAll()
+    // res.json(data)
+  } catch (err) {
+    next({ status: 422, message: 'this is horrible' })
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+  try {
+    res.json('get accounts id')
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
+  try {
+    res.json('post accounts')
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+router.put('/:id',
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  md.checkAccountId, async (req, res, next) => {
+    try {
+      res.json('put accounts')
+    } catch (err) {
+      next(err)
+    }
+  });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+  try {
+    res.json('delete accounts')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+//2. connect to accounts router!!
+const accountsRouter = require('./accounts/accounts-router');
+
 const server = express();
 
 server.use(express.json());
 
+//3. CONNECT (BELOW JSON)!!
+server.use('/api/accounts', accountsRouter);
+
+//1. wiring our catch all :)
 server.use('*', (req, res) => {
     res.status(404).json({ message: 'not found!!' })
-})
+});
 
 module.exports = server;
